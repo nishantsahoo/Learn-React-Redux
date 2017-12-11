9523b4cd5778c64af197b6de4a3709c6e72c56ba
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-12-12 05:00:05 +0530

    Actions complete

--- dev/js/containers/user-list.js
+++ dev/js/containers/user-list.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+import {selectUser} from '../actions/index';
 
 class UserList extends Component {
 
 	renderItems() {
 		return this.props.users.map((user => {
 			return (
-				<li key={user.id}>{user.first} {user.last}</li>
+				<li 
+				key={user.id}
+				onClick={() => this.props.selectUser(user)}
+				>
+					{user.first} {user.last}
+				</li>
 			)
 		})
 		)
@@ -30,6 +36,10 @@ function mapStateToProps(state) {
 	};
 }
 
-export default connect(mapStateToProps)(UserList);
+function matchDispatchToProps(dispatch) {
+	return bindActionCreators({selectUser: selectUser}, dispatch)
+}
+
+export default connect(mapStateToProps, matchDispatchToProps)(UserList);
 
 // ^ Makes your container aware about the store
