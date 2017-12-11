621015afe05cd9bfe8f4f63e6326c6dcf96d66ad
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-12-12 03:59:27 +0530

    Reducers complete

--- dev/js/reducers/index.js
+++ dev/js/reducers/index.js
@@ -1,3 +1,8 @@
 // This file will be used to combine all the reducers (data) that goes into the store
 
 import {combineReducers} from "redux";
+import UserReducer from "./reducer-users";
+
+const allReducers = combineReducers({
+	users: UserReducer
+});
